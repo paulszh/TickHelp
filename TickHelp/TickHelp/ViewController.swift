95c5ec4d6ef179678f9017a8bd169113a51048b3
Ariel <arieeel1110@gmail.com>
2016-04-09 16:15:16 -0700

    Proc init

---
+++ TickHelp/TickHelp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TickHelp
+//
+//  Created by Ariel on 4/9/16.
+//  Copyright © 2016 Ariel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
