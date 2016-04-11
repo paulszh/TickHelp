8424aed569d06615b05e72b54a7e9ae953168ebb
Ariel <arieeel1110@gmail.com>
2016-04-10 17:56:28 -0700

    Log in UI

--- TickHelp/TickHelp/LoginViewController.swift
+++ TickHelp/TickHelp/LoginViewController.swift
@@ -8,18 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+import VideoSplashKit
 
+class ViewController: VideoSplashViewController {
+    
+    @IBOutlet weak var LogIn: UIButton!
+    @IBOutlet weak var SignUp: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //Video
+        let url = NSURL.fileURLWithPath(NSBundle.mainBundle().pathForResource("test2", ofType: "mp4")!)
+        self.videoFrame = view.frame
+        self.fillMode = .ResizeAspectFill
+        self.alwaysRepeat = true
+        self.sound = true
+        self.startTime = 0.0
+        self.duration = 8.0
+        self.alpha = 0.4
+        self.backgroundColor = UIColor.blackColor()
+        self.contentURL = url
+        self.restartForeground = true
+        
+        
+        
+        //Buttons
+        LogIn.layer.cornerRadius = 5
+        LogIn.layer.borderWidth = 1
+        LogIn.layer.borderColor = UIColor.whiteColor().CGColor
+
+        
+        SignUp.layer.cornerRadius = 5
+        SignUp.layer.borderWidth = 1
+        SignUp.layer.borderColor = UIColor.whiteColor().CGColor
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
