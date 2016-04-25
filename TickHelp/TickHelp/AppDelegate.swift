3dd72cdd681e31c61dc6a41ead9413a0eea2cc29
Ariel <arieeel1110@gmail.com>
2016-04-24 18:48:15 -0700

    Revert ""Moving things around.""
    This reverts commit 3000975ac54f8bb0d42525b0ed75c1f5bb464ba5.

--- TickHelp/TickHelp/AppDelegate.swift
+++ TickHelp/TickHelp/AppDelegate.swift
@@ -13,12 +13,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
     
-    var mpcManager: MPCManager?
+    var mpcManager: MPCManager!
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
        
-        //mpcManager = MPCManager()
+        mpcManager = MPCManager()
         return true
     }
 
--- TickHelp/TickHelp/LogViewController.swift
+++ TickHelp/TickHelp/LogViewController.swift
@@ -15,8 +15,6 @@ class LogViewController: UIViewController {
     @IBOutlet weak var username: UITextField!
     @IBOutlet weak var password: UITextField!
     var ref = Firebase(url: constant.userURL)
-    let appDelagate = UIApplication.sharedApplication().delegate as! AppDelegate
-  //  var mpcManager: MPCManager!
         
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,6 +23,7 @@ class LogViewController: UIViewController {
     }
     
     func setPlacehoder(){
+        
         let placeholder1 = NSAttributedString(string: "User name", attributes: [NSForegroundColorAttributeName : UIColor.lightGrayColor()])
 
         let placeholder2 = NSAttributedString(string: "Password", attributes: [NSForegroundColorAttributeName : UIColor.lightGrayColor()])
@@ -43,7 +42,6 @@ class LogViewController: UIViewController {
                 //let alert = UIAlertController(title: "", message: "Please check your username and password", preferredStyle: UIAlertControllerStyle.Alert)
                 //self.presentViewController(alert, animated: true, completion: nil)
             } else {
-                self.appDelagate.mpcManager = MPCManager()
                 // Authentication just completed successfully :)
                 // The logged in user's unique identifier
                 print(authData.uid)
